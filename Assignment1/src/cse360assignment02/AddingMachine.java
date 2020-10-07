a75d5ec77d8e2f737693bb627c3f87133c8ba067
bvong <bvong@LAPTOP-5JCG6T4Q>
2020-10-07 00:47:22 -0700

    Check of Adding Machine.

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,55 @@
+package cse360assignment02;
+
+/**
+ * Defines global variables.
+ * @author bvongsachang
+ *
+ */
+public class AddingMachine {
+  private int total;
+  
+  
+  /**
+   * Global variable instantiation.
+   */
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  
+  /**
+   * Method to get modified variable.
+   * @return
+   */
+  public int getTotal () {
+    return 0;
+  }
+  
+  
+  /**
+   * Method that adds parameter value to total.
+   * @param value
+   */
+  public void add (int value) {
+  }
+  
+  /**
+   * Method that subtracts parameter value from total.
+   * @param value
+   */
+  public void subtract (int value) {
+  }
+
+  /**
+   * Method that converts the values of total to a desired string.
+   */
+  public String toString () {
+    return "";
+  }
+  
+  /**
+   * Method that clears the value of total.
+   */
+  public void clear() {
+  }
+}
---
+++ Assignment1/src/cse360assignment02/Main.java
@@ -0,0 +1,27 @@
+package cse360assignment02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		AddingMachine myCalculator = new AddingMachine();
+
+
+		
+		//myCalculator.getTotal(); 
+	
+		myCalculator.add (4); 
+
+		myCalculator.subtract (2); 
+
+		myCalculator.add(5);
+
+		System.out.println(myCalculator.toString());
+
+		
+		
+	}
+
+}
